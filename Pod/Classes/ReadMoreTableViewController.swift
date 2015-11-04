2b78dea0971e51b011e0bdc1fba6e8935f717381
mishimay <mishimay@istyle.co.jp>
2015-11-04 14:48:04 +0900

    Improve performance with using insertRowsAtIndexPaths

--- Pod/Classes/ReadMoreTableViewController.swift
+++ Pod/Classes/ReadMoreTableViewController.swift
@@ -10,7 +10,7 @@ public class ReadMoreTableViewController: UITableViewController {
     private var hidesFooter = false
     private var mainCellCount = 0
     private var allCellCount: Int {
-        return topCells.count + mainCellCount + (hidesFooter ? 0 : 1)
+        return topCells.count + mainCellCount
     }
 
     public var configureCellClosure: (cell: UITableViewCell, row: Int) -> UITableViewCell = { cell, row in return cell }
@@ -34,8 +34,16 @@ public class ReadMoreTableViewController: UITableViewController {
 
     // MARK: - TableViewDataSource
 
-    override public func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return allCellCount
+    public override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 2
+    }
+
+    public override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return allCellCount
+        } else {
+            return (hidesFooter ? 0 : 1)
+        }
     }
 
     public override func tableView(tableView: UITableView, estimatedHeightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
@@ -46,13 +54,13 @@ public class ReadMoreTableViewController: UITableViewController {
         }
     }
 
-    override public func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        if isTopCell(indexPath) {
-            return topCells[indexPath.row]
-        } else if isFooter(indexPath) {
+    public override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        if isFooter(indexPath) {
             let cell = tableView.dequeueReusableCellWithIdentifier(readMoreCellIdentifier, forIndexPath: indexPath)
             cell.separatorInset = UIEdgeInsets(top: 0, left: CGFloat.max, bottom: 0, right: 0) // cf. http://stackoverflow.com/questions/8561774/hide-separator-line-on-one-uitableviewcell
             return cell
+        } else if isTopCell(indexPath) {
+            return topCells[indexPath.row]
         } else {
             let cell = tableView.dequeueReusableCellWithIdentifier(mainCellIdentifier, forIndexPath: indexPath)
             return configureCellClosure(cell: cell, row: indexPath.row - topCells.count)
@@ -61,19 +69,30 @@ public class ReadMoreTableViewController: UITableViewController {
 
     // MARK: - TableViewDelegate
 
-    override public func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+    public override func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
         cellHeights[indexPath] = cell.frame.height
 
         if isFooter(indexPath) {
             let currentCount = mainCellCount
+            let currentAllCellCount = allCellCount
+
             dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) {
                 self.fetchReadCountClosure(currentCount: currentCount, completion: { [weak self] readCount, hasNext in
+
                     dispatch_async(dispatch_get_main_queue()) {
-                        if !hasNext {
-                            self?.hidesFooter = true
-                        }
                         self?.mainCellCount = currentCount + readCount
-                        self?.tableView.reloadData()
+                        UIView.setAnimationsEnabled(false)
+                        self?.tableView.insertRowsAtIndexPaths(
+                            Array(currentAllCellCount..<currentAllCellCount + readCount).map{ NSIndexPath(forRow: $0, inSection: 0) },
+                            withRowAnimation: .None)
+                        UIView.setAnimationsEnabled(true)
+
+                        if !hasNext {
+                            self?.updateFooter(false)
+                        } else {
+                            // To call willDisplayCell delegate to read cells
+                            self?.updateFooter(true)
+                        }
                     }
                 })
             }
@@ -87,11 +106,14 @@ public class ReadMoreTableViewController: UITableViewController {
     }
 
     public func clearData() {
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(0.01 * Double(NSEC_PER_SEC))), dispatch_get_main_queue()) {
-            self.tableView.scrollToRowAtIndexPath(NSIndexPath(forRow: 0, inSection: 0), atScrollPosition: .Top, animated: false)
-        }
         mainCellCount = 0
-        hidesFooter = false
+        tableView.reloadData()
+        updateFooter(true)
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(0.01 * Double(NSEC_PER_SEC))), dispatch_get_main_queue()) {
+            if self.tableView(self.tableView, numberOfRowsInSection: 1) > 0  { // Prevent error "row (0) beyond bounds (0) for section (0)."
+                self.tableView.scrollToRowAtIndexPath(NSIndexPath(forRow: 0, inSection: 1), atScrollPosition: .Top, animated: false)
+            }
+        }
     }
 
     // MARK: - Private
@@ -101,7 +123,25 @@ public class ReadMoreTableViewController: UITableViewController {
     }
 
     private func isFooter(indexPath: NSIndexPath) -> Bool {
-        return indexPath.row == topCells.count + mainCellCount
+        return indexPath.section == 1
+    }
+
+    private func updateFooter(show: Bool) {
+        if show && hidesFooter {
+            UIView.setAnimationsEnabled(false)
+            hidesFooter = false
+            tableView.insertRowsAtIndexPaths([NSIndexPath(forRow: 0, inSection: 1)], withRowAnimation: .Fade)
+            UIView.setAnimationsEnabled(true)
+
+        } else if !show && !hidesFooter {
+            hidesFooter = true
+            tableView.deleteRowsAtIndexPaths([NSIndexPath(forRow: 0, inSection: 1)], withRowAnimation: .Fade)
+
+        } else if show && !hidesFooter {
+            UIView.setAnimationsEnabled(false)
+            tableView.reloadRowsAtIndexPaths([NSIndexPath(forRow: 0, inSection: 1)], withRowAnimation: .Fade)
+            UIView.setAnimationsEnabled(true)
+        }
     }
 
 }
