a01ed4feedee3fb5476ad7b215a47497053123b2
mishimay <mishimay@istyle.co.jp>
2016-05-11 12:02:17 +0900

    Edit Tests

---
+++ Example/Tests/Tests.swift
@@ -0,0 +1,18 @@
+import XCTest
+import LoadMoreTableViewController
+
+class ExampleTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+
+    func testExample() {
+        XCTAssert(true)
+    }
+
+}
